015d6358d0b123fdadbb24483a181c2c13413b1e
alex5n <silveracuri@gmail.com>
2021-02-04 13:01:47 -0500

    Alumno

--- src/app/admin/alumno/alumno.component.ts
+++ src/app/admin/alumno/alumno.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators ,ReactiveFormsModule } from '@angular/forms';
+import { AgGridAngular } from 'ag-grid-angular';
 
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
@@ -8,9 +9,53 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./alumno.component.css']
 })
 export class AlumnoComponent implements OnInit {
+  @ViewChild('agGrid') agGrid: AgGridAngular;
+
+  private gridApi;
+  private gridColumnApi;
+
+  columnDefs;
+  defaultColDef;
+  rowData: any;
+
   
   formAlumno:FormGroup;
-  constructor(private formBuilder:FormBuilder,private modal:NgbModal) { }
+  constructor(private formBuilder:FormBuilder,private modal:NgbModal) {
+    this.columnDefs = [
+      { 
+        headerName: "Nivel", 
+        field: 'nivel', 
+        sortable: true, 
+        checkboxSelection: true,
+        suppressSizeToFit: true,
+        width: 100,
+      },
+      { 
+        headerName: "Apellido Paterno", 
+        field: 'apellidopaterno', 
+        sortable: true, 
+        checkboxSelection: true, 
+      },
+      { 
+        headerName: "Apellido Materno", 
+        field: 'apellidomaterno', 
+        sortable: true 
+      },
+      { 
+        headerName: "Nombres", 
+        field: 'nombres', 
+        sortable: true 
+      },
+    ];
+    this.defaultColDef = { resizable: true };
+   }
+
+   onFirstDataRendered(params) {
+    params.api.sizeColumnsToFit();
+  }
+  OnGridReady(params ){
+    this.gridApi=params.api;
+  }
   saveCurso(){
     
     
